1c5a19e7ec415aabbd32f5c5e2e305663963080c
bambaleyla <ivanivchatov@gmail.com>
2022-03-24 22:21:30 +0300

    center.js is done

--- src/models/center.js
+++ src/models/center.js
@@ -1,3 +1,15 @@
 export default class Center{
+    constructor(data) {
+        this.init(data)
+    }
+
+    init({ id = '', name = '', phone = '', addition = '', created_at = '', updated_at = ''}) {
+        this.id = id
+        this.name = name
+        this.phone = phone
+        this.addition = addition
+        this.created_at = created_at
+        this.updated_at = updated_at
+    }
 
 }
